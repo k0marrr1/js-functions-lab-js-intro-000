fe566db2074e91cf262f0ee6fa58b6fa4d5463d3
omar hussain <omar.hussain4991@gmail.com>
2019-09-15 20:02:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ function happyHolidays(string) {
 function happyHolidaysTo(name) {
   return "Happy holidays, you!"
 }
-function happyHolidayTo (holiday, name) {
+function happyHolidayTo(holiday, name) {
   return "Happy Independence Day, you!"
+}
+function holidayCountdown(holiday, days){
+  return "It\'s ${days} days until ${holiday}"
 }
